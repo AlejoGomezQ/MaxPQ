f89af8e580a95feaed218685ef734309e43a23cf
AlejoGomezQ <alejandro.gomezq97@gmail.com>
2024-04-16 21:06:45 -0500

    Swim, swap, resize, insert and print methods created

---
+++ src/MaxPQ.java
@@ -0,0 +1,87 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class MaxPQ {
+
+    Integer[] heap;
+    int n;
+
+    public MaxPQ(int capacity) {
+        heap = new Integer[capacity + 1];
+        n = 0;
+    }
+
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void insert(int x) {
+        if(n == heap.length -1) {
+            resize(2*heap.length);
+        }
+        heap[++n] = x;
+        swim(n);
+    }
+
+    private void resize(int capacity) {
+        Integer[] temp = new Integer[capacity];
+        for(int i = 0; i < heap.length; i++) {
+            temp[i] = heap[i];
+        }
+        heap = temp;
+    }
+
+    private void swim(int k) {
+        while(k > 1 && heap[k/2] < heap[k]) {
+            int temp = heap[k];
+            heap[k] = heap[k/2];
+            heap[k/2] = temp;
+            k = k/2;
+        }
+    }
+
+
+    private void sink(int k) {
+
+    }
+
+    public int deleteMax() {
+        int max = heap[1];
+        swap(1, n);
+        n--;
+        sink(1);
+        heap[n + 1] = null;
+        if(n > 0 && (n == (heap.length - 1) / 4)) {
+            resize(heap.length / 2);
+        }
+
+        return max;
+    }
+
+    public void swap(int a, int b) {
+        int temp = heap[a];
+        heap[a] = heap[b];
+        heap[b] = temp;
+    }
+
+    public void print() {
+        for(int i = 1; i <= n; i++) {
+            System.out.println(heap[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        MaxPQ pq = new MaxPQ(3);
+        pq.insert(4);
+        pq.insert(5);
+        pq.insert(2);
+        pq.insert(6);
+        pq.insert(1);
+        pq.insert(3);
+        System.out.println("MaxPQ size: " + pq.size());
+        pq.print();
+    }
+}
